092d34a27b5c26663d81dee21db748270404d038
Enot <natasha.cat982404@gmail.com>
2019-11-25 13:38:55 +0200

    Added second button

--- WFApp/WFApp/Form1.cs
+++ WFApp/WFApp/Form1.cs
@@ -21,5 +21,10 @@ namespace WFApp
         {
             MessageBox.Show("enot");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("kurlik");
+        }
     }
 }
