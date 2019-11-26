4c7c8293eb0b213649bec2768cb014c17a52d6ef
Dima <dfdfdf@gmail.com>
2019-11-26 13:18:09 +0200

    Added Button

--- WinFormsApp/WinFormsApp/Form1.cs
+++ WinFormsApp/WinFormsApp/Form1.cs
@@ -16,5 +16,10 @@ namespace WinFormsApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dmitry");
+        }
     }
 }
